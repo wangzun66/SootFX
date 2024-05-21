247af491cbed477a0962470546de258024271d9f
zunhoho <wangzun666@gmail.com>
2024-05-21 09:02:09 +0200

    fix errors

--- src/main/java/core/fx/methodstmtbased/ProportionOfRelevantStmtsBeforeSS.java
+++ src/main/java/core/fx/methodstmtbased/ProportionOfRelevantStmtsBeforeSS.java
@@ -1,5 +1,7 @@
 package core.fx.methodstmtbased;
 
+import com.google.common.graph.GraphBuilder;
+import com.google.common.graph.MutableGraph;
 import core.fx.base.Feature;
 import core.fx.base.MethodStmtFEU;
 import soot.SootMethod;
@@ -20,9 +22,15 @@ public class ProportionOfRelevantStmtsBeforeSS implements MethodStmtFEU<Double> 
      * @param tail  query stmt
      * @return
      */
-    @Override
+
     public Feature<Double> extract(SootMethod target, Stmt tail) {
-        int count = getRelevantStmtsCount(target, tail);
+        DirectedGraph<Unit> graph = new BriefUnitGraph(target.getActiveBody());
+        MutableGraph<Unit> visited = GraphBuilder.directed().build();
+        Map<Unit, Integer> unit2Num = new HashMap<>();
+        for(Unit head : graph.getHeads()){
+            convertUnitsToNum(graph, head, visited, 0, unit2Num);
+        }
+        int count = getRelevantStmtsCount(unit2Num, tail);
         int methodSize = target.getActiveBody().getUnits().size();
         if(methodSize == 0){
             methodSize = 1;
@@ -31,35 +39,42 @@ public class ProportionOfRelevantStmtsBeforeSS implements MethodStmtFEU<Double> 
         return new Feature<>(this.getClass().getSimpleName(), Math.floor(proportion*10 + 0.5)/10);
     }
 
-    private int getRelevantStmtsCount(SootMethod target, Stmt stmt){
-        Set<Unit> visited = new HashSet<>();
+    private int getRelevantStmtsCount(Map<Unit, Integer> stmt2Num, Stmt stmt){
+        int base = stmt2Num.get(stmt);
         Set<Unit> relevantStmts = new HashSet<>();
-        DirectedGraph<Unit> graph = new BriefUnitGraph(target.getActiveBody());
-        Deque<Unit> unitsInCurrentDepth = new ArrayDeque<>(graph.getHeads());
-        List<Unit> unitInNextDepth = new ArrayList<>();
-        while(!unitsInCurrentDepth.isEmpty()){
-            Unit unit = unitsInCurrentDepth.removeFirst();
-            visited.add(unit);
-            if(unit instanceof Stmt){
-                if(unit instanceof JIdentityStmt){
-                    relevantStmts.add(unit);
-                }else if(((Stmt) unit).containsInvokeExpr()){
-                    relevantStmts.add(unit);
-                }else if(unit instanceof JAssignStmt){
-                    relevantStmts.add(unit);
-                }
-            }
-            List<Unit> succs = graph.getSuccsOf(unit);
-            for(Unit succ : succs){
-                if(!visited.contains(succ)){
-                    unitInNextDepth.add(succ);
-                }
-            }
-            if(unitsInCurrentDepth.isEmpty() && !unitInNextDepth.contains(stmt)){
-                unitsInCurrentDepth.addAll(unitInNextDepth);
-                unitInNextDepth.clear();
+        for(Map.Entry<Unit, Integer> entry : stmt2Num.entrySet()){
+            if(entry.getValue() < base && isRelevantStmt(entry.getKey())){
+                relevantStmts.add(entry.getKey());
             }
         }
         return relevantStmts.size();
+    }
+
+    private boolean isRelevantStmt(Unit unit){
+        if(unit instanceof Stmt){
+            if(unit instanceof JIdentityStmt){
+                return true;
+            }else if(((Stmt) unit).containsInvokeExpr()){
+                return true;
+            }else if(unit instanceof JAssignStmt){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void convertUnitsToNum(DirectedGraph<Unit> graph, Unit curr, MutableGraph<Unit> visited, Integer depth, Map<Unit, Integer> unit2Num){
+        Integer num = unit2Num.get(curr);
+        if(num == null || num < depth){
+            unit2Num.put(curr, depth);
+        }
+        depth++;
+        List<Unit> succsOf = graph.getSuccsOf(curr);
+        for (Unit succ : succsOf) {
+            if (!visited.hasEdgeConnecting(curr, succ) && !curr.equals(succ)) {
+                visited.putEdge(curr, succ);
+                convertUnitsToNum(graph, succ, visited, depth, unit2Num);
+            }
+        }
     }
 }
--- src/main/java/core/fx/methodstmtbased/StmtDepthProportion.java
+++ src/main/java/core/fx/methodstmtbased/StmtDepthProportion.java
@@ -1,5 +1,7 @@
 package core.fx.methodstmtbased;
 
+import com.google.common.graph.GraphBuilder;
+import com.google.common.graph.MutableGraph;
 import core.fx.base.Feature;
 import core.fx.base.MethodStmtFEU;
 import soot.SootMethod;
@@ -17,39 +19,36 @@ public class StmtDepthProportion implements MethodStmtFEU<Double> {
      * @param tail  query stmt
      * @return
      */
+    int maxDepth = -1;
+
     @Override
     public Feature<Double> extract(SootMethod target, Stmt tail) {
-        Set<Unit> visited = new HashSet<>();
-        int depth = 0;
-        int tailDepth = 0;
         DirectedGraph<Unit> graph = new BriefUnitGraph(target.getActiveBody());
-        Deque<Unit> unitsInCurrentDepth = new ArrayDeque<>(graph.getHeads());
-        List<Unit> unitInNextDepth = new ArrayList<>();
-        while(!unitsInCurrentDepth.isEmpty()){
-            depth++;
-            Unit unit = unitsInCurrentDepth.removeFirst();
-            visited.add(unit);
-            if(unit instanceof Stmt){
-                Stmt stmt = (Stmt) unit;
-                if(stmt.equals(tail)){
-                    tailDepth = depth;
-                }
-            }
-            List<Unit> succs = graph.getSuccsOf(unit);
-            for(Unit succ : succs){
-                if(!visited.contains(succ)){
-                    unitInNextDepth.add(succ);
-                }
-            }
-            if(unitsInCurrentDepth.isEmpty()){
-                unitsInCurrentDepth.addAll(unitInNextDepth);
-                unitInNextDepth.clear();
-            }
+        MutableGraph<Unit> visited = GraphBuilder.directed().build();
+        Map<Unit, Integer> unit2Num = new HashMap<>();
+        this.maxDepth = -1;
+        for(Unit head : graph.getHeads()){
+            convertUnitsToNum(graph, head, visited, 0, unit2Num);
         }
-        if(depth == 0){
-            depth++;
-        }
-        Double proportion = (double) tailDepth / (double) depth ;
+        Double proportion = (double) unit2Num.get(tail) / (double) maxDepth ;
         return new Feature<>(this.getClass().getSimpleName(), Math.floor((proportion*10) + 0.5)/10);
+    }
+
+    private void convertUnitsToNum(DirectedGraph<Unit> graph, Unit curr, MutableGraph<Unit> visited, Integer depth, Map<Unit, Integer> unit2Num){
+        if(depth>maxDepth){
+            maxDepth = depth;
+        }
+        Integer num = unit2Num.get(curr);
+        if(num == null || num < depth){
+            unit2Num.put(curr, depth);
+        }
+        depth++;
+        List<Unit> succsOf = graph.getSuccsOf(curr);
+        for (Unit succ : succsOf) {
+            if (!visited.hasEdgeConnecting(curr, succ) && !curr.equals(succ)) {
+                visited.putEdge(curr, succ);
+                convertUnitsToNum(graph, succ, visited, depth, unit2Num);
+            }
+        }
     }
 }
